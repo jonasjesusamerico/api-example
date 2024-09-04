b4bd275f323cd2b3573e586c9540b1ea16aa55e1
jonasjesusamerico <jjsamerico@gmail.com>
2024-09-04 07:01:17 -0300

    Inicio

---
+++ src/main/kotlin/br/com/jonasdev/api_wallet/ApiWalletApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jonasdev.api_wallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiWalletApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiWalletApplication>(*args)
+}
